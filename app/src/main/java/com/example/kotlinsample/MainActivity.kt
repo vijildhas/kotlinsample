3a175df9ab044aeb91f079f9821201425f3a0054
Vijil Dhas <vijildhas.as@experionglobal.com>
2022-04-07 13:13:01 +0530

    initial

---
+++ app/src/main/java/com/example/kotlinsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ui.SplashFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
